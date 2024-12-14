84f1f299c54f9ae3a16d8f397ab914cc6d56911d
desmond123-arch <desmondopokumends2004@gmail.com>
2024-12-14 17:08:23 +0000

    imports

--- Frontend/ITG-frontend/src/App.tsx
+++ Frontend/ITG-frontend/src/App.tsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
 import './App.css'
-import { Button } from './components/ui/button'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from "@/pages/Home.tsx";
-import About from './pages/About';
 import MainLayout from './components/MainLayout';
 import Error404Page from './pages/page404';
 import routes from './routes';
