ee812e0fd2666074f55d20390b2fec712da3ef97
Andy Ansong <andyansong82@gmail.com>
2025-03-19 00:57:28 +0000

    complete filter

--- Frontend/ITG-frontend/src/components/ui/HomeUI/Filters.tsx
+++ Frontend/ITG-frontend/src/components/ui/HomeUI/Filters.tsx
@@ -2,45 +2,65 @@ import React from 'react'
 import { Input } from '@/components/ui/input'
 import { Switch } from "@/components/ui/switch"
 import { Checkbox } from "@/components/ui/checkbox"
+import FilterTitle from './FilterTitle'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 const Filters: React.FC = () => {
   return (
     <div className='flex flex-col gap-3'>
       <h1 className='text-xl font-semibold'>Filter</h1>
-      <div className='rounded-md bg-white'>
-        {
-          Array.from({length: 2}).map((_item, index) => (
-            <div className=''>
-              <span>Location</span>
-              <Input placeholder='City'/>
-            </div>
-          ))
-        }
-        <div>
-          <span>Remote Worker</span>
-          <Switch/>
+      <div className='rounded-md bg-white shadow-md px-5 py-3 w-[200px]'>
+        <div className='flex flex-col gap-1 border-b pb-4'>
+          <FilterTitle title='Location'/>
+          <Input placeholder='City'/>
         </div>
-        <div>
-          <span>Type Works</span>
-          {
-            Array.from({length: 4}).map((_item, index) => (
-              <div key={index}>
-                <Checkbox/>
-                <span>Intenship</span>
-              </div>
-            ))
-          }
+        <div className='flex flex-col gap-1 border-b py-4'>
+          <FilterTitle title='Show By'/>
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Relevant"/>
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="latest">Latest</SelectItem>
+              <SelectItem value="oldest">Oldest</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
-        <div>
-          <span>Experience</span>
-          {
-            Array.from({length: 4}).map((_item, index) => (
-              <div key={index}>
-                <Checkbox/>
-                <span>Fresh Graduate</span>
-              </div>
-            ))
-          }
+        <div className='py-4 border-b items-center flex justify-between'>
+          <FilterTitle title='Remote Worker'/>
+          <Switch className='bg-green-500'/>
+        </div>
+        <div className='py-4 border-b flex flex-col gap-2'>
+          <FilterTitle title='Type Works'/>
+          <div>
+            {
+              Array.from({length: 4}).map((_item, index) => (
+                <div key={index} className='flex items-center gap-2'>
+                  <Checkbox/>
+                  <span>Intenship</span>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <div className='py-4 flex flex-col gap-2'>
+        <FilterTitle title='Experience'/>
+          <div>
+            {
+              Array.from({length: 4}).map((_item, index) => (
+                <div key={index} className='flex items-center gap-2'>
+                  <Checkbox/>
+                  <span>Fresh Graduate</span>
+                </div>
+              ))
+            }
+          </div>
         </div>
       </div>
     </div>
