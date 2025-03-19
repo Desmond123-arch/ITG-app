0e04859e3062ce5e2221f00d1389c4ca593fe527
desmond123-arch <desmondopokumends2004@gmail.com>
2025-03-19 16:25:25 +0000

    clean up and added arias

--- Frontend/ITG-frontend/src/pages/Job.tsx
+++ Frontend/ITG-frontend/src/pages/Job.tsx
@@ -33,70 +33,70 @@ const JobDescription = () => {
         <div className="flex justify-between lg:flex-row flex-col">
             {/* JOB DETAIL */}
             <div className='w-full md:w-[90%] mx-auto lg:w-[68%] flex flex-col gap-5'>
-                <div className="flex flex-wrap bg-white rounded-md px-5 py-2 gap-6 items-start">
+                <div className="flex flex-wrap bg-white rounded-md px-5 py-2 gap-6 items-center">
                     <div className='flex items-center gap-4'>
                         <div className="w-14 h-14 flex-shrink-0">
-                            <img src={ItgIcon || "/placeholder.svg"} className="w-full h-full rounded-full object-center object-cover" />
+                            <img src={ItgIcon || "/placeholder.svg"} className="w-full h-full rounded-full object-center object-cover"  alt='Company logo'/>
                         </div>
                         <div className='flex flex-col'>
                             <h2 className='text-2xl font-semibold mb-2'>{jobDetail.title}</h2>
-                            <ul className='flex gap-x-3 text-sm flex-wrap'>
-                                <li>Omicron</li>
-                                <li className='flex items-center h-max gap-1'><span className='bg-slate-600 h-1 w-1 rounded-full '></span>{jobDetail.location}</li>
-                                <li className='flex items-center h-max gap-1'><span className='bg-slate-600 h-1 w-1 rounded-full '></span>{jobDetail.jobType}</li>
-                                <li className='flex items-center h-max gap-1'><span className='bg-slate-600 h-1 w-1 rounded-full '></span>{jobDetail.experience}</li>
-                            </ul>
+                            <dl className='flex gap-x-3 text-sm flex-wrap'>
+                                <div>Omicron</div>
+                                <div className='flex items-center h-max gap-1'><dt className='bg-slate-600 h-1 w-1 rounded-full '></dt><dd>{jobDetail.location}</dd></div>
+                                <div className='flex items-center h-max gap-1'><dt className='bg-slate-600 h-1 w-1 rounded-full '></dt><dd>{jobDetail.jobType}</dd></div>
+                                <div className='flex items-center h-max gap-1'><dt className='bg-slate-600 h-1 w-1 rounded-full '></dt><dd>{jobDetail.experience}</dd></div>
+                            </dl>
                         </div>
                     </div>
                     <div className='ml-auto flex items-center gap-2 w-[90%] md:w-[20%]'>
-                        <Bookmark color='gray' size={25} className='hover:cursor-pointer md: order-1 sm:order-2' />
-                        <Button className='p-5 w-[80%] md:order-2 sm:order-1'>Apply</Button>
+                        <Bookmark color='gray' size={25} className='hover:cursor-pointer md: order-1 sm:order-2' aria-label="Save job to bookmarks"/>
+                        <Button className='p-5 w-[80%] md:order-2 sm:order-1' aria-label='Apply for job'>Apply</Button>
                     </div>
                 </div>
                 <div className='bg-white rounded-md p-5 flex flex-col gap-4'>
                     {/* Job descriptions */}
-                    <div>
+                    <section>
                         <h3 className='text-md font-semibold text-gray-700'>Job Descriptions</h3>
-                        <ul className='list-disc ml-4 mt-3'>
+                        <ul className='list-disc ml-4 mt-3' role='list'>
                             {jobDetail.description.map(job => (
                                 <li className='flex items-center h-max gap-2'><span className='bg-gray-500 h-1 w-1 rounded-full '></span>{job}</li>
                             ))}
                         </ul>
-                    </div>
+                    </section>
 
                     {/* Skills */}
-                    <div>
+                    <section>
                         <h3 className='text-md font-semibold text-gray-700 mb-3 mt-3'>Skills</h3>
-                        <ul className='flex gap-2 ml-2'>
+                        <ul className='flex gap-2 ml-2' role='list'>
                             {jobDetail.skills.map(skill => (
                                 <li className='bg-blue-200 rounded-md p-1 px-2 text-[#28246F]'>{skill}</li>
                             ))}
                         </ul>
-                    </div>
+                    </section>
 
                     {/* Requirements */}
-                    <div>
+                    <section>
                         <h3 className='text-md font-semibold text-gray-700 mt-4'>Requirements</h3>
                         <ul className='list-disc ml-4 mt-3'>
                             {jobDetail.description.map(job => (
                                 <li className='flex items-center h-max gap-2'><span className='bg-gray-500 h-1 w-1 rounded-full '></span>{job}</li>
                             ))}
                         </ul>
-                    </div>
+                    </section>
 
                     {/* About Company */}
-                    <div>
+                    <section>
                         <h3 className='text-md font-semibold text-gray-700 mt-4' A>About Company</h3>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore non amet reprehenderit asperiores accusantium hic, veritatis, pariatur ipsam minus omnis explicabo beatae recusandae aperiam, assumenda iure quaerat? Odio, quae obcaecati?
                         Fuga voluptatibus dolore numquam mollitia aperiam assumenda libero? Ipsam pariatur eos dolor tempora quisquam officia incidunt voluptatem debitis, accusantium repudiandae quia quasi quas itaque odit esse a neque aspernatur ullam.
-                    </div>
+                    </section>
                 </div>
             </div>
 
             {/* RELATED JOBS */}
             <div className='lg:mr-1 lg:w-[30%] w-full'>
                 <h3 className='text-md font-bold text-gray-700 mt-4 md:mt-0 self-baseline ml-4 md:ml-0'>Related jobs</h3>
-                <div className="grid sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-1  gap-3 mt-3 mx-auto md:w-full place-items-center">
+                <div className="grid sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-1 gap-3 mt-3 mx-auto md:w-full place-items-center">
                     {jobs.slice(0, 3).map((job, index) => (
                         <JobItem key={index} job={job} />
                     ))}
