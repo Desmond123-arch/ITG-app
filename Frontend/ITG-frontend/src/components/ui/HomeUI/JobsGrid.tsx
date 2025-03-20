f95d4b34b3fcd0bcb889e844125d72717a5838ff
Andy Ansong <andyansong82@gmail.com>
2025-03-20 03:51:31 +0000

    style job grid

--- Frontend/ITG-frontend/src/components/ui/HomeUI/JobsGrid.tsx
+++ Frontend/ITG-frontend/src/components/ui/HomeUI/JobsGrid.tsx
@@ -4,7 +4,7 @@ import jobs from '@/data/JobsData'
 
 const JobsGrid: React.FC = () => {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
+    <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-3'>
       {
         jobs.map((job, index) => (
             <JobItem job={job} key={index}/>
