0f39762a6742d99a60ae181387df4e77c0102811
Andy Ansong <andyansong82@gmail.com>
2025-06-28 02:56:56 +0100

    center loader

--- Frontend/ITG-frontend/src/components/ui/CustomLoader.tsx
+++ Frontend/ITG-frontend/src/components/ui/CustomLoader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CustomLoader: React.FC = () => {
     return (
-        <div className="flex min-h-screen w-full items-center justify-center bg-gray-100">
+        <div className="flex min-h-screen items-center justify-center absolute w-full h-full bottom-0 left-0">
             <div className="flex flex-col items-center gap-3">
                 <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
                 <p className="text-gray-700 font-semibold text-lg">Loading, please wait...</p>
