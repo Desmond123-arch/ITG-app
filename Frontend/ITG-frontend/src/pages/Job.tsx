09a6b22f29f8fd420cf1edb7b19a244b02716972
Andy Ansong <andyansong82@gmail.com>
2025-07-04 01:53:55 +0100

    refactor query data

--- Frontend/ITG-frontend/src/pages/Job.tsx
+++ Frontend/ITG-frontend/src/pages/Job.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@/components/ui/button';
 import CustomLoader from '@/components/ui/CustomLoader';
 import JobItem from '@/components/ui/HomeUI/JobItem';
-import jobs from '@/data/JobsData';
 import { RootState } from '@/store';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
@@ -68,15 +67,15 @@ const JobDescription = () => {
                 <div className="flex flex-wrap bg-white rounded-md px-5 py-2 gap-6 items-center">
                     <div className='flex items-center gap-4'>
                         <div className="w-14 h-14 flex-shrink-0">
-                            <img src={data.job.companyLogo} className="w-full h-full rounded-full object-center object-cover"  alt='Company logo'/>
+                            <img src={data.current_job.job.companyLogo} className="w-full h-full rounded-full object-center object-cover"  alt='Company logo'/>
                         </div>
                         <div className='flex flex-col'>
-                            <h2 className='text-2xl font-semibold mb-2'>{data.job.title}</h2>
+                            <h2 className='text-2xl font-semibold mb-2'>{data.current_job.job.title}</h2>
                             <dl className='flex gap-x-3 text-sm flex-wrap'>
-                                <div>{data.job.companyName}</div>
-                                <div className='flex items-center h-max gap-1'><dt className='bg-slate-600 h-1 w-1 rounded-full '></dt><dd>{data.job.location}</dd></div>
-                                <div className='flex items-center h-max gap-1'><dt className='bg-slate-600 h-1 w-1 rounded-full '></dt><dd>{data.job.jobType.replace('_', ' ')}</dd></div>
-                                <div className='flex items-center h-max gap-1'><dt className='bg-slate-600 h-1 w-1 rounded-full '></dt><dd>{data.job.yearsOfExperience}</dd></div>
+                                <div>{data.current_job.job.companyName}</div>
+                                <div className='flex items-center h-max gap-1'><dt className='bg-slate-600 h-1 w-1 rounded-full '></dt><dd>{data.current_job.job.location}</dd></div>
+                                <div className='flex items-center h-max gap-1'><dt className='bg-slate-600 h-1 w-1 rounded-full '></dt><dd>{data.current_job.job.jobType.replace('_', ' ')}</dd></div>
+                                <div className='flex items-center h-max gap-1'><dt className='bg-slate-600 h-1 w-1 rounded-full '></dt><dd>{data.current_job.job.yearsOfExperience}</dd></div>
                             </dl>
                         </div>
                     </div>
@@ -90,7 +89,7 @@ const JobDescription = () => {
                     <section>
                         <h3 className='text-md font-semibold text-gray-700'>Job Descriptions</h3>
                         <ul className='list-disc ml-4 mt-3' role='list'>
-                            {data.job.description.map((info: string, index: number) => (
+                            {data.current_job.job.description.map((info: string, index: number) => (
                                 <li className='flex items-center h-max gap-2' key={index}><span className='bg-gray-500 h-1 w-1 rounded-full '></span>{info}</li>
                             ))}
                         </ul>
@@ -100,7 +99,7 @@ const JobDescription = () => {
                     <section>
                         <h3 className='text-md font-semibold text-gray-700 mb-3 mt-3'>Skills</h3>
                         <ul className='flex gap-2 ml-2' role='list'>
-                            {data.job.skills.map((skill: string, index: number) => (
+                            {data.current_job.job.skills.map((skill: string, index: number) => (
                                 <li className='bg-blue-200 rounded-md p-1 px-2 text-[#28246F]' key={index}>{skill}</li>
                             ))}
                         </ul>
@@ -110,7 +109,7 @@ const JobDescription = () => {
                     <section>
                         <h3 className='text-md font-semibold text-gray-700 mt-4'>Requirements</h3>
                         <ul className='list-disc ml-4 mt-3'>
-                            {data.job.requirements.map((detail: string, index: number) => (
+                            {data.current_job.job.requirements.map((detail: string, index: number) => (
                                 <li className='flex items-center h-max gap-2' key={index}><span className='bg-gray-500 h-1 w-1 rounded-full '></span>{detail}</li>
                             ))}
                         </ul>
@@ -119,7 +118,7 @@ const JobDescription = () => {
                     {/* About Company */}
                     <section>
                         <h3 className='text-md font-semibold text-gray-700 mt-4'>About Company</h3>
-                        {data.job.companyDescription}
+                        {data.current_job.job.companyDescription}
                     </section>
                 </div>
             </div>
@@ -128,7 +127,7 @@ const JobDescription = () => {
             <div className='lg:mr-1 lg:w-[30%] w-full'>
                 <h3 className='text-md font-bold text-gray-700 mt-4 md:mt-0 self-baseline md:ml-0'>Related jobs</h3>
                 <div className="flex hidden_scrollbar rounded-lg overflow-x-scroll md:grid sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-1 gap-3 mt-3 mx-auto md:w-full place-items-stretch">
-                    {jobs.slice(0, 3).map((job, index) => (
+                    {data.jobs_list.slice(0, 3).map((job, index) => (
                         <JobItem key={index} job={job} page="job" />
                     ))}
                 </div>
