15d7c95f83edc31e12d5ba52e179e1ddd5c6de7b
Desmond123-arch <desmondopokumends2004@gmail.com>
2025-07-13 16:46:44 +0000

    hotfix:removed bg color

--- Frontend/ITG-frontend/src/pages/PostJob.tsx
+++ Frontend/ITG-frontend/src/pages/PostJob.tsx
@@ -2,7 +2,7 @@ import { JobForm } from "@/components/ui/Forms/postJob";
 
 const PostAJob = () => {
     return (
-        <div className="w-full min-h-screen bg-gray-100 pb-10 overflow-x-hidden bg-red-500">
+        <div className="w-full min-h-screen bg-gray-100 pb-10 overflow-x-hidden">
             <div className="mt-7 w-full max-w-xl mx-auto flex flex-col shadow-md px-6 py-8 rounded-md bg-white">
                 <JobForm/>
             </div>
